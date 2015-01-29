b083f37b4809c4535631ebfce789d8400db6b17b
sharyari <sharyari@gmail.com>
2015-01-29 20:05:32 +0100

    Fixed the broken code.

--- lab1/libpedsim/ped_model.cpp
+++ lab1/libpedsim/ped_model.cpp
@@ -16,33 +16,33 @@ const std::vector<Ped::Tagent*> Ped::Model::getAgents() const
   return agents;
 }
 
-void tickHelp(std::vector<Ped::Tagent*> agents2)
+void* tickHelp(void *arg)
 {
+  std::vector<Ped::Tagent*> agents2 = *((std::vector<Ped::Tagent*> *)arg);
   for(std::vector<Ped::Tagent*>::iterator it = agents2.begin(); it != agents2.end();++it){
-    Ped::Tagent* temp = *it;
-    temp->whereToGo();
-    temp->go();
+    (*it)->whereToGo();
+    (*it)->go();
   }
-  // EDIT HERE
 }
 
 void Ped::Model::tick(){
-  int numProc = 2;
+  int numProc = 1;
   int blocksize = agents.size()/numProc;
-  int n=0;
   pthread_t threads[numProc];
-  for (int i=0;i<numProc;i++){
-    std::vector<Ped::Tagent*> agents2;
-    std::cout << "a" << endl;    
-    for(std::vector<Ped::Tagent*>::iterator it = agents.begin()+i*blocksize; !(it == agents.end() || it==agents.begin()+(i+1)*blocksize);++it){
-      n++;
- std::cout << n << endl;       
-	 agents2.push_back(*it);
-  }
-    void * result;
-    pthread_create(&threads[i], NULL, &tickHelp,NULL);
-    pthread_join(threads[i], &result);
- }
+  std::vector<Ped::Tagent*> agents2[numProc];
 
+  for (int i=0;i<numProc;i++){
+    for(std::vector<Ped::Tagent*>::iterator it = agents.begin()+i*blocksize;
+	!(it == agents.end() || it==agents.begin()+(i+1)*blocksize);
+	++it)
+      {
+    	agents2[i].push_back(*it);
+      }
+    pthread_create(&threads[i], NULL, &tickHelp,(void*) &agents2[i]);
+  }
+  void * result;
+  for (int i=0; i< numProc;i++)
+    pthread_join(threads[i], &result);
+     
 }
 
